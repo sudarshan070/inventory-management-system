922c06b3c8b6310593eba6c4c151303480f746d9
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-09-30 00:28:16 +0530

    order-list

--- server/routes/order.js
+++ server/routes/order.js
@@ -5,15 +5,11 @@ var Product = require('../models/product')
 
 router.post('/', async (req, res, next) => {
     try {
-        console.log(req.body);
         var product = await Product.findById(req.body.product)
-        console.log(product,"hawa");
         if(product.quantity > req.body.quantity){
             req.body.status="confirmed"
             let order=await Order.create(req.body)
-            console.log(order,"order placed");
             var updatedProduct=await Product.findByIdAndUpdate(product.id,{$inc:{quantity:-req.body.quantity}},{new:true})
-            console.log(updatedProduct,"update product");
             res.status(201).json({success:true,order})
         }else{
             console.log("not enough quantity");
@@ -23,5 +19,16 @@ router.post('/', async (req, res, next) => {
         next(error)
     }
 })
+
+router.get('/',async(req,res,next)=>{
+    try {
+        let orderList=await Order.find({}).populate("product").exec();
+        console.log(orderList,"all list order");
+        res.status(201).json({orderList})
+    } catch (error) {
+        next(error);
+    }
+})
+
 
 module.exports = router;
